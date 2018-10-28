1ef21ee1b9249f31f990d826b5282eab04fc4903
sunlggggg <sunlggggg@gmail.com>
2018-10-28 23:22:50 +0800

    rabbitmq demo

---
+++ client.py
@@ -0,0 +1,33 @@
+# -# -*- coding: UTF-8 -*-
+#!/usr/bin/env python
+import pika
+import requests
+import requests
+
+r = requests.get("https://cache.zhibo8.cc/json/2018_10_25/news/nba/130734_hot.htm")
+# 创建一个连接
+connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost')) 
+#创建通道
+channel = connection.channel() 
+#把消息队列的名字为comment
+channel.queue_declare(queue='comment')
+channel.basic_publish(exchange='', routing_key='comment',body=r.text.decode('unicode_escape')) 
+print r.text.decode('unicode_escape')
+print(" [x] Sent Comment!")
+*- coding: UTF-8 -*-
+#!/usr/bin/env python
+import pika
+import requests
+import requests
+
+r = requests.get("https://cache.zhibo8.cc/json/2018_10_25/news/nba/130734_hot.htm")
+# 创建一个连接
+connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost')) 
+#创建通道
+channel = connection.channel() 
+#把消息队列的名字为comment
+channel.queue_declare(queue='comment')
+channel.basic_publish(exchange='', routing_key='comment',body=r.text.decode('unicode_escape')) 
+print r.text.decode('unicode_escape')
+print(" [x] Sent Comment!")
+
